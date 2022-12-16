de24432593718fcc256e71c5f3c07210b3ac6b2c
Karin Centenaro <centenarokarin7@gmail.com>
2022-12-16 17:13:24 -0300

    add words to the dictionary language list

--- dic_espaniol_portgues.py
+++ dic_espaniol_portgues.py
@@ -1,6 +1,109 @@
-espaniol_a_portgues={
+espaniol_a_portgues= {
     "Cuchillo":"Faca",
-    "Cuchara":"Colher"
+    "Cuchara":"Colher",
+    "Sufrir": "Sofrer",
+    "Como":"Cómo",
+    "Eu":"Yo",
+    "Buenos días, buenas tardes" : "Bom dia, Boa tarde",
+    "Hola" : "Oi",
+    "Buenas noches" : "Boa noite",
+    "¿Cómo estás?" : "Tudo Bem?" ,
+    "¿Qué tal?" : "Como vai?",
+    "Muy bien, gracias" : "Muito bem, obrigado ",
+    "Adiós" : "Tchau, Adeus ",   
+    "Hasta pronto" :" Até já ",
+    "Encantado(a)" :" Muito prazer ",
+    "Me llamo xxx": 'Me chamo xxx o Meu nome é ""',
+No hablo (bien) portugués = (Eu) não falo (bem) português 
+¿Puede hablar más despacio, por favor? = Pode falar mais devagar, por favor?
+Hablo un poco de portugués = (Eu) falo um pouco de português 
+¿Puede repetirlo, por favor? = Pode repetir, por favor?
+Fórmulas de cortesía
+Disculpe, perdón = desculpe, com licença 
+Un placer = com prazer 
+No pasa nada = não importa 
+Es usted muy amable = O Senhor / A Senhora é muito gentil
+Diviértase = Divirta-se 
+Buena suerte = Boa sorte 
+Algunos adjetivos
+abierto(a) = aberto o aberta 
+cerrado(a) = fechado ou fechada 
+caliente = quente 
+frío = frio ou fria
+nuevo(a) = novo / nova 
+pequeño(a) = pequeno / pequena 
+fácil = fácil 
+difícil = difícil 
+lleno = cheio 
+ocupado = ocupado 
+Algunos sustantivos
+el nombre = o nome
+la ciudad = a cidade 
+la entrada = a entrada 
+la salida = a saída 
+el hombre o el chico = o homem, o menino 
+la mujer o la chica = a mulher, a menina  
+la gente = as pessoas 
+el horario = o horário 
+el lugar = o lugar 
+la parada = a parada 
+Algunos verbos
+comer = comer 
+beber = beber / tomar 
+dormir = dormir 
+ir = ir 
+poder = poder / ser capaz de 
+ayudar = ajudar 
+comprar = comprar 
+gustar = gostar 
+tener = ter 
+hablar = falar  
+hacer = fazer 
+Los primeros pasos en portugués
+Comer y beber
+Tengo hambre = Estou com fome 
+Tengo sed = Estou com sede 
+el desayuno = o café da manhã
+la comida o almuerzo = o almoço 
+la cena = o jantar 
+el vaso, la copa = o copo 
+¿Qué le gustaría comer? = o que você gostaria de comer?
+¿Cuál es la especialidad de la casa? = qual é a especialidade da casa?
+la carta, el menú = a cardápio, o menu 
+la mesa = a mesa 
+¿Me puede recomendar un restaurante? = pode me recomendar um restaurante?
+Viajes: transporte y alojamiento
+el viaje = a viagem 
+la salida / la llegada = a partida / a chegada 
+¿Dónde va? = Aonde você vai? 
+Bajo en la siguiente parada, por favor = Desço na próxima parada, por favor 
+una cama individual / una cama doble = uma cama de solteiro / uma cama de casal 
+Me gustaría reservar una noche más = Gostaria de reservar uma noite a mais 
+¿A qué hora tengo que dejar la habitación? = A que horas devo liberar o quarto?
+Tengo sueño, necesito ir a dormir = estou com sono, preciso ir dormir
+el edredón = o cobertor
+la almohada = o travesseiro 
+Jerga brasileña
+Este es un pequeño avance de nuestro artículo sobre jerga brasileña. Algunas palabras te resultarán útiles a la hora de hablar con nativos o durante un viaje a Brasil.
+
+"chico" :"cara",
+"los chicos" : "a galera"
+"extranjero" : "gringo, gringa" 
+"¡muy guay! " : "isso é demais! / muito legal!",
+"no es verdad", 
+"¡te burlas de mí!" :"Tá me gozando";
+"En caso de urgencia":"Para Emergencias",
+"¡Socorro! / ¡Ayúdenme! = socorro! / me ajude!",
+"Necesito su ayuda" : "preciso da sua ajuda",
+"Ayúdeme a llamar a una ambulancia" = "me ajude a chamar uma ambulância",
+"Es una emergencia" = "é uma emergência ",
+"He perdido el pasaporte" = "eu perdi meu passaporte", 
+"He perdido la maleta" = "eu perdi minha mala",
+"estar en una situación difícil" = "entrar numa fria (jerga, informal)",
+"enfermo" : "doente "
+"Necesito un médico urgentemente" = preciso de um médico urgente 
+"el dolor" :"a dor "",
+
 }
 
 for espaniol in espaniol_a_portgues:
